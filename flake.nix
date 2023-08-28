cd91d900c5599b72db284f599931f92dcb6f4b55
Payton Redemer <paytonredemer@gmail.com>
2023-08-27 23:14:26 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      t480s = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-        modules = [ ./nixos/configuration.nix ];
+        modules = [
+          ./hosts/t480s/configuration.nix
+          nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+          nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
+          nixos-hardware.nixosModules.common-gpu-intel
+        ];
       };
     };
   };
