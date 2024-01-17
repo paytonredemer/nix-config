47b20ae30ba803847c8eeb5ed5dcff138d901a14
Payton Redemer <paytonredemer@gmail.com>
2024-01-17 05:32:03 +0000

    rename stuff pt 2

--- home-manager/modules/cli/neovim/nvim/lua/plugins/zen.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/zen-mode.nvim",
-  dependencies = "folke/twilight.nvim",
-  cmd = "ZenMode",
-  opts = {},
-  keys = {
-    { "<leader>uz", "<cmd>ZenMode<cr>", desc = "[U]i [Z]en toggle" },
-  },
-}
