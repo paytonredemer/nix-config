1ac09af40ae02c65c4b772f1e5310b9dcb2ba436
Payton Redemer <paytonredemer@gmail.com>
2024-01-19 21:57:10 +0000

    update treesitter config

--- home/modules/cli/neovim/nvim/lua/plugins/treesitter.lua
+++ home/modules/cli/neovim/nvim/lua/plugins/treesitter.lua
@@ -3,14 +3,15 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     -- A list of parser names, or "all"
-    ensure_installed = { "vimdoc", "c", "cpp", "python", "bash", "lua", "rust", "r", "javascript", "typescript" },
+    ensure_installed = {},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+    -- NOTE: Handled by nix
+    auto_install = false,
 
     highlight = {
       -- `false` will disable the whole extension
