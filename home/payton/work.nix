0d8594bf1a7340963d92f47574b7d4fd81582e67
Payton Redemer <paytonredemer@gmail.com>
2024-04-12 13:18:10 -0500

    update work git config

---
+++ home/payton/work.nix
@@ -0,0 +1,32 @@
+{ inputs, config, pkgs, lib, ... }:
+
+{
+  home.username = "redemerpayton";
+  home.homeDirectory = "/home/redemerpayton";
+
+  home.stateVersion = "23.11";
+
+  imports = [
+    ../modules/cli/git
+    ../modules/cli/shell.nix
+    ../modules/cli/fish.nix
+    ../modules/cli/starship.nix
+    ../modules/cli/neovim
+    ../modules/cli/tmux.nix
+    ../modules/cli/lf.nix
+    ../modules/cli/zsh.nix
+  ];
+
+  # Enable unfree packages
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = (_: true);
+  };
+
+  nixpkgs.overlays = [
+    inputs.neovim-nightly-overlay.overlay
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
