da2915253d21706d12a4bda9ae0e75081522e9b9
Payton Redemer <paytonredemer@gmail.com>
2024-05-08 17:21:33 +0000

    add diagnostics keymaps

--- home/modules/cli/neovim/nvim-config/lua/config/keymaps.lua
+++ home/modules/cli/neovim/nvim-config/lua/config/keymaps.lua
@@ -33,3 +33,19 @@ vim.keymap.set("n", "Q", "@q")
 vim.keymap.set("x", "Q", ":norm @q<CR>")
 
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- diagnostics
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "[C]ode [D]iagnostics" })
+vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
