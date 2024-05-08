a7c903a943c40f5e019f90069cd84dd590a5dba5
Payton Redemer <paytonredemer@gmail.com>
2024-05-08 14:29:16 +0000

    add syncthing for work

---
+++ modules/syncthing.nix
@@ -0,0 +1,9 @@
+{
+  services.syncthing = {
+    enable = true;
+    guiAddress = "127.0.0.1:8385";
+    user = "redemerpayton";
+    group = "wheel";
+    dataDir = "/home/redemerpayton/";
+  };
+}
