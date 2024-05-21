2a9720e9fd13e2c708593accbc70bb01ddabd88c
Payton Redemer <paytonredemer@gmail.com>
2024-05-21 20:13:16 +0000

    add org mode plugin

---
+++ home/modules/cli/neovim/nvim-config/lua/plugins/org.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  ft = { "org" },
+  opts = {
+    org_agenda_files = "~/Documents/org/**/*",
+    org_default_notes_file = "~/Documents/org/refile.org",
+  },
+}
