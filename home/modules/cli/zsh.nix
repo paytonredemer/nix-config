bbdd8ced35860be7f69d72531614ab15ac886023
Payton Redemer <paytonredemer@gmail.com>
2024-06-05 19:59:06 +0000

    add abbrs in zsh

--- home/modules/cli/zsh.nix
+++ home/modules/cli/zsh.nix
@@ -1,5 +1,29 @@
+# TODO: Add vim bindings and other nice to haves in zsh
+let
+  editor = "nvim";
+in
 {
   programs.zsh = {
     enable = true;
+    zsh-abbr = {
+      enable = true;
+      abbreviations = {
+        e = editor;
+        v = editor;
+        g = "git";
+        h = "history";
+      };
+    };
+    shellAliases = {
+      ls = "ls -hN --color=auto --group-directories-first";
+      grep = "grep --color=auto";
+      cp = "cp -iv";
+      mv = "mv -iv";
+      rm = "rm -Iv";
+      diff = "diff --color=auto";
+      ":q" = "exit";
+      ":wq" = "exit";
+    };
+
   };
 }
