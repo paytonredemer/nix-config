f60ff24f6e3e2a6425cf3d57bc38c05eaecabfcd
Payton Redemer <paytonredemer@gmail.com>
2024-06-14 14:30:36 +0000

    update lazy loading nvim

--- home/modules/cli/neovim/nvim-config/lua/plugins/org_telescope.lua
+++ home/modules/cli/neovim/nvim-config/lua/plugins/org_telescope.lua
@@ -1,16 +1,33 @@
--- TODO: Fix key conflicts and labels. Lazy loading and rename file?
-return  {
-    "nvim-orgmode/telescope-orgmode.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-orgmode/orgmode",
-      "nvim-telescope/telescope.nvim",
+return {
+  "nvim-orgmode/telescope-orgmode.nvim",
+  dependencies = {
+    "nvim-orgmode/orgmode",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("telescope").load_extension("orgmode")
+  end,
+  keys = {
+    {
+      "<leader>or",
+      function()
+        require("telescope").extensions.orgmode.refile_heading()
+      end,
+      desc = "[O]rg [R]efile heading",
     },
-    config = function()
-      require("telescope").load_extension("orgmode")
-
-      vim.keymap.set("n", "<leader>os", require("telescope").extensions.orgmode.refile_heading)
-      vim.keymap.set("n", "<leader>oh", require("telescope").extensions.orgmode.search_headings)
-      vim.keymap.set("n", "<leader>ol", require("telescope").extensions.orgmode.insert_link)
-    end,
-  }
+    {
+      "<leader>oh",
+      function()
+        require("telescope").extensions.orgmode.search_headings()
+      end,
+      desc = "[O]rg search [H]eadings",
+    },
+    {
+      "<leader>ol",
+      function()
+        require("telescope").extensions.orgmode.insert_link()
+      end,
+      desc = "[O]rg insert [L]ink",
+    },
+  },
+}
